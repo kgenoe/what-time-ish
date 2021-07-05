9a84e0ed3628982e2f6691a5b32d47ed742a1a5a
Kyle Genoe <kylegenoe@hotmail.com>
2021-07-04 22:20:34 -0400

    Fix time formatting issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { useLocation } from "react-router-dom";
 
 function App() {
   return (
@@ -25,18 +23,30 @@ function dateForTimeOfDay(timeOfDay) {
   const minute = randomNumber(0, 59);
 
   if (timeOfDay === 'morning') {
+    // 5am-11:59am
     const hour = randomNumber(5, 11);
     return new Date(2001, 0 , 1, hour, minute); 
+
   } else if (timeOfDay === 'afternoon') {
-    const hour = randomNumber(12, 4);
+    // 12pm-4:49pm
+    const hour = randomNumber(12, 16);
     return new Date(2001, 0 , 1, hour, minute); 
+
   } else if (timeOfDay === 'evening') {
-    const hour = randomNumber(5, 9);
+    // 5pm-9:59pm
+    const hour = randomNumber(17, 21);
     return new Date(2001, 0 , 1, hour, minute); 
+
   } else if (timeOfDay === 'night') {
-    const hour = randomNumber(10, 4);
+    // 10pm-4:59am
+    var hour = randomNumber(22, 28);
+    if (hour >= 24) {
+      hour -= 24
+    }
     return new Date(2001, 0 , 1, hour, minute); 
+
   } else {
+    // All Hours
     const hour = randomNumber(0, 23);
     return new Date(2001, 0 , 1, hour, minute); 
   }
@@ -44,7 +54,7 @@ function dateForTimeOfDay(timeOfDay) {
 
 
 function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 export default App;
