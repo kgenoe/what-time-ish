44e27796a049bb3d2687b1c45e9f749ef909712d
Kyle Genoe <kylegenoe@hotmail.com>
2021-07-04 20:07:09 -0400

    Add basic time routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useLocation } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>idk Kate, it's around {whatTimeIshIt(window.location.pathname).toString()}</p>
       </header>
     </div>
   );
+}
+
+
+function whatTimeIshIt(input) {
+  const timeOfDay = input.substr(1);
+  var date = dateForTimeOfDay(timeOfDay);
+  return date.toLocaleTimeString('en-US', { hour12: true, hour: "numeric", minute: "numeric"});
+} 
+
+function dateForTimeOfDay(timeOfDay) {
+  // 1.01.2011, 02:03
+  // Date(2011, 0 , 1, 2, 3);  
+  const minute = randomNumber(0, 59);
+
+  if (timeOfDay === 'morning') {
+    const hour = randomNumber(5, 11);
+    return new Date(2001, 0 , 1, hour, minute); 
+  } else if (timeOfDay === 'afternoon') {
+    const hour = randomNumber(12, 4);
+    return new Date(2001, 0 , 1, hour, minute); 
+  } else if (timeOfDay === 'evening') {
+    const hour = randomNumber(5, 9);
+    return new Date(2001, 0 , 1, hour, minute); 
+  } else if (timeOfDay === 'night') {
+    const hour = randomNumber(10, 4);
+    return new Date(2001, 0 , 1, hour, minute); 
+  } else {
+    const hour = randomNumber(0, 23);
+    return new Date(2001, 0 , 1, hour, minute); 
+  }
+}
+
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
 export default App;
